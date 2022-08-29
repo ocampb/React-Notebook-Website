e367747532df2a7b3f404d5d6fa329489e8da456
Olivia Campbell <106823996+ocampb@users.noreply.github.com>
2022-08-29 12:59:31 -0500

    cart and header working

--- notebook-website/src/App.js
+++ notebook-website/src/App.js
@@ -1,23 +1,19 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Homepage from "./components/homepage";
+import { render } from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PLP from "./components/PLP";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/products/notebooks" element={<PLP />} />
+          <Route path="/products/planners" element={<PLP />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ notebook-website/src/components/homepage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header2 from "./header2";
+import Preview from "./Preview";
+import { Link } from "react-router-dom";
+
+export default function Homepage() {
+  return (
+    <div>
+      <Header2 />
+      <Preview />
+    </div>
+  );
+}
