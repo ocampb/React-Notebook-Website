947c3d0cb5c0f8879b5ef7c19be2002fa44fc0f6
Olivia Campbell <106823996+ocampb@users.noreply.github.com>
2022-08-30 08:56:37 -0500

    state is working

--- notebook-website/src/components/PLP_pens.js
+++ notebook-website/src/components/PLP_pens.js
@@ -16,48 +16,10 @@
 */
 
 import Header2 from "../components/header2";
-
-const products = [
-  {
-    id: 1,
-    name: "Pen1",
-    href: "#",
-    price: "$48",
-    imageSrc: "/images/opensource/pen6.jpeg",
-    imageAlt:
-      "Tall slender porcelain bottle with natural clay textured body and cork stopper.",
-  },
-  {
-    id: 2,
-    name: "Pen2",
-    href: "#",
-    price: "$35",
-    imageSrc: "/images/opensource/pen2.jpeg",
-    imageAlt:
-      "Olive drab green insulated bottle with flared screw lid and flat top.",
-  },
-  {
-    id: 3,
-    name: "Pen3",
-    href: "#",
-    price: "$89",
-    imageSrc: "/images/opensource/pen3.jpeg",
-    imageAlt:
-      "Person using a pen to cross a task off a productivity paper card.",
-  },
-  {
-    id: 4,
-    name: "Pen4",
-    href: "#",
-    price: "$35",
-    imageSrc: "/images/opensource/pen5.jpeg",
-    imageAlt:
-      "Hand holding black machined steel mechanical pencil with brass tip and top.",
-  },
-  // More products...
-];
-
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 export default function Example() {
+  const products = useSelector((state) => state.products);
   return (
     <div className="bg-white">
       <Header2 />
@@ -65,21 +27,23 @@ export default function Example() {
         <h2 className="sr-only">Products</h2>
 
         <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-          {products.map((product) => (
-            <a key={product.id} href={product.href} className="group">
-              <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
-                <img
-                  src={product.imageSrc}
-                  alt={product.imageAlt}
-                  className="w-full h-full object-center object-cover group-hover:opacity-75"
-                />
-              </div>
-              <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
-              <p className="mt-1 text-lg font-medium text-gray-900">
-                {product.price}
-              </p>
-            </a>
-          ))}
+          {products
+            .filter((product) => product.category === "Pen")
+            .map((product) => (
+              <a key={product.id} href={product.href} className="group">
+                <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+                  <img
+                    src={product.imageSrc}
+                    alt={product.imageAlt}
+                    className="w-full h-full object-center object-cover group-hover:opacity-75"
+                  />
+                </div>
+                <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
+                <p className="mt-1 text-lg font-medium text-gray-900">
+                  {product.price}
+                </p>
+              </a>
+            ))}
         </div>
       </div>
     </div>
